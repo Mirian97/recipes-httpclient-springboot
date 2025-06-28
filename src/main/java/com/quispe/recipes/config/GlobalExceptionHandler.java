2881ae47a7e971eb3d07f5bbe68dc2aa4adcb35c
Mirian Quispe <mirianquispe1505@gmail.com>
2025-06-27 21:41:41 -0300

    Refactor GlobalExceptionHandler to return standardized ErrorResponse DTO for exceptions

--- src/main/java/com/quispe/recipes/config/GlobalExceptionHandler.java
+++ src/main/java/com/quispe/recipes/config/GlobalExceptionHandler.java
@@ -5,13 +5,24 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.quispe.recipes.domain.dto.ErrorResponse;
 import com.quispe.recipes.domain.exception.ExternalApiException;
+import com.quispe.recipes.domain.exception.RecipeNotFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
   @ExceptionHandler(ExternalApiException.class)
-  public ResponseEntity<String> handleExternalApiException(ExternalApiException e) {
-    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+  public ResponseEntity<ErrorResponse> handleExternalApiException(ExternalApiException e) {
+    return ResponseEntity
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .body(new ErrorResponse(e.getMessage()));
+  }
+
+  @ExceptionHandler(RecipeNotFoundException.class)
+  public ResponseEntity<ErrorResponse> handleRecipeNotFoundException(RecipeNotFoundException e) {
+    return ResponseEntity
+        .status(HttpStatus.NOT_FOUND)
+        .body(new ErrorResponse(e.getMessage()));
   }
 }
